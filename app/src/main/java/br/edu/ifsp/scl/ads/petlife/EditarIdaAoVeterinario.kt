be9efab200195328b78165eebe6311e23b0107f0
kaykyjuvenal <neguito.juvenal@gmail.com>
2024-10-21 09:56:43 -0300

    feat: include EditarPetActivity class

--- app/src/main/java/br/edu/ifsp/scl/ads/petlife/EditarIdaAoVeterinario.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/petlife/EditarIdaAoVeterinario.kt
@@ -16,12 +16,22 @@ class EditarIdaAoVeterinarioActivity : AppCompatActivity() {
 
         binding.salvarBt.setOnClickListener {
             val nomeCachorro = binding.nomeCachorroEt.text.toString()
-            val novaData = binding.dataVeterinarioEt.text.toString()
+            val novoNomeCachorro = binding.novoNomeCachorroEt.text.toString()
+            val novaDataNascimento = binding.novaDataNascimentoEt.text.toString()
+            val novoTipo = binding.novoTipoEt.text.toString()
+            val novaCor = binding.novaCorEt.text.toString()
+            val novoPorte = binding.novoPorteEt.text.toString()
 
-            if (nomeCachorro.isNotEmpty() && novaData.isNotEmpty()) {
+            if (nomeCachorro.isNotEmpty() && novoNomeCachorro.isNotEmpty() && novaDataNascimento &&
+                novoTipo.isNotEmpty() && novaCor.isNotEmpty() && novoPorte.isNotEmpty()) {
                 val resultIntent = intent.apply {
                     putExtra("nomeCachorro", nomeCachorro)
-                    putExtra("novaDataVeterinario", novaData)
+                    putExtra("novoNomeCachorro", novoNomeCachorro)
+                    putExtra("novaDataNascimento", novaDataNascimento)
+                    putExtra("novoTipo", novoTipo)
+                    putExtra("novaCor", novaCor)
+                    putExtra("novoPorte", novoPorte)
+
                 }
                 setResult(Activity.RESULT_OK, resultIntent)
                 finish()
